5f3613c0a2e85b19417834e51a969663ac38eb3e
Aminadnan2022 <Aminadnan2022@users.noreply.github.com>
2026-09-02 00:45:16 +0000

    Supabase backup

--- prisma/backups/data.sql
+++ prisma/backups/data.sql
@@ -4,7 +4,7 @@ SET session_replication_role = replica;
 -- PostgreSQL database dump
 --
 
--- \restrict EhFgHz4kZcp841gNjrOfiNgsWozS8gneqYQVNzpVhbdoTbd5XfX3uSC29n2elTs
+-- \restrict Dnw7fuzhNuwyFhE55ogWkcHQC6OhcZG2uUzo06WoeGbHLCtux5PzgYFAUXdCLPt
 
 -- Dumped from database version 17.6
 -- Dumped by pg_dump version 17.6
@@ -679,6 +679,6 @@ SELECT pg_catalog.setval('"public"."suppliers_id_seq"', 1, true);
 -- PostgreSQL database dump complete
 --
 
--- \unrestrict EhFgHz4kZcp841gNjrOfiNgsWozS8gneqYQVNzpVhbdoTbd5XfX3uSC29n2elTs
+-- \unrestrict Dnw7fuzhNuwyFhE55ogWkcHQC6OhcZG2uUzo06WoeGbHLCtux5PzgYFAUXdCLPt
 
 RESET ALL;
